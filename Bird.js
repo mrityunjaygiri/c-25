6dc293d652a4aa9c0d3e516aa13372541c3d1ae0
mrityunjaygiri <78855357+mrityunjaygiri@users.noreply.github.com>
2021-05-06 12:58:32 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,15 @@
+class Bird extends BaseClass{     //bird = new Bird(100,100);
+  constructor(x, y) {
+    super(x, y, 50, 50);
+    this.image = loadImage("sprites/bird.png");
+   
+  }
+  display(){
+   var pos = this.body.position;
+    pos.x = mouseX
+    pos.y= mouseY;
+
+    super.display();
+   
+  }
+};
